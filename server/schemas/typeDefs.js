832d0ddcdad3ad387d2adc7fb56ed84d23b46dc4
Mary <itsmaryyang@gmail.com>
2023-05-05 20:25:11 -0500

    Add code to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,15 +5,26 @@ const typeDefs = 'gql'`
         _id: ID!
         username: String!
         email: String!
-        password: String!
         bookCount: Int
         savedBooks: [Book]
     }
     type Book{
-        bookId: String!
-        authors: String
+        bookId: ID!
+        authors: [String]
         description: String!
         title: String!
+        image: String
+        link: String
+    }
+    type Auth{
+        token: ID!
+        user: User!
+    }
+    input BookInput{
+        authors: [String]
+        description: String!
+        title: String!
+        bookId: String!
         image: String
         link: String
     }
@@ -21,7 +32,10 @@ const typeDefs = 'gql'`
         me: User
     }
     type Mutation{
-        
+        login(email:String!, password:String!): Auth
+        addUser(username:String!, email:String!, password:String): Auth
+        saveBook(bookInput:BookInput!):User
+        removeBook(bookId:ID!):User
     }
 `;
 
