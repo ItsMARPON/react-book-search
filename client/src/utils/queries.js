7bb92644ae1593f695bd909b415989e42b0b1d83
Mary <itsmaryyang@gmail.com>
2023-05-09 12:17:51 -0500

    Remove QUERY_USER not  needed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-    query user($username: String!){
-        user(username: $username) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
-    }
-`;
 
 export const QUERY_ME = gql`
     query me {
